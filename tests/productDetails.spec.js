66974e7b55643fbab05d9826f2b5de0067fe853c
Leonidas <leonidas.drawanz@gmail.com>
2022-01-21 18:54:57 -0300

    dois requisitos no teste 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,8 +34,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // Teste se o retorno da função é um array.
     expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBe(true)
     // Teste se o array retornado pela função contém dois itens dentro.
-
+    expect((productDetails('Alcool gel', 'Máscara')).length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('Alcool gel', 'Máscara')[0] && (typeof productDetails('Alcool gel', 'Máscara')[1])).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
     // Teste se os dois productIds terminam com 123.
   });
